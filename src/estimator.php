4e034ae89638f2644d44d9044f545b7a49186af5
GabrielFemi <gabrielfemi799@gmail.com>
2020-04-11 13:44:17 +0100

    created function to supply data

--- src/estimator.php
+++ src/estimator.php
@@ -10,3 +10,35 @@ function covid19ImpactEstimator($data)
 {
   return $data;
 }
+// For errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/**
+ * Gives out the Data.
+ */
+function covidData()
+{
+  
+  $region = "";
+  $data = [
+    "region" =>  [
+      "name" => "Africa",
+      "avgAge" => 19.7,
+      "avgDailyIncomeInUSD" => 5,
+      "avgDailyIncomePopulation" => 0.71,
+    ],
+    "periodType" => "days",
+    "timeToElapse" => 58,
+    "reportedCases" => 674,
+    "population" => 66622705,
+    "totalHospitalBeds" => 1380614
+  ];
+  return json_decode(json_encode($data));
+}
+
+echo covidData()->region->name;
+
+
+// For errors
+
