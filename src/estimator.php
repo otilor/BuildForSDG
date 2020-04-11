6aae6ce645cd97f240b8b776d81a3475e1b8ef46
GabrielFemi <gabrielfemi799@gmail.com>
2020-04-11 19:44:09 +0100

    finished challenge-1

--- src/estimator.php
+++ src/estimator.php
@@ -2,12 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-/**
- * Estimates the impact of Covid19
- * 
- * @param string $data
- * @return string $data|null
- */
+
 
 
 class estimator {
